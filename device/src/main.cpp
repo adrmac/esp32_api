070d937449393b5ee531ffe743e1fa5f808de231
Adrian <adrmac@gmail.com>
2026-07-21 11:43:39 -0700

    Expose dashboard for router proxy

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -559,6 +559,7 @@ void startServices() {
   ArduinoOTA.onError([](ota_error_t) { otaInProgress = false; });
   ArduinoOTA.begin();
   server.on("/", []() { server.sendHeader("Location", "https://adrian-pi:3000/indoor-sky/", true); server.send(302, "text/plain", "Dashboard moved to signal-router"); });
+  server.on("/dashboard", []() { server.send_P(200, "text/html", DASHBOARD_HTML); });
   server.on("/status", []() { server.sendHeader("Cache-Control", "no-store"); server.sendHeader("Connection", "close"); server.send(200, "application/json", statusJson()); server.client().stop(); });
   server.on("/audio/raw", []() { if (server.hasArg("enabled")) { String value = server.arg("enabled"); setPcmStreamEnabled(value == "1" || value == "true" || value == "on"); } server.send(200, "application/json", String("{\"enabled\":") + (pcmStreamEnabled ? "true" : "false") + ",\"auto_disabled\":" + (pcmAutoDisabled ? "true" : "false") + "}"); });
   server.on("/audio/usb", []() {
